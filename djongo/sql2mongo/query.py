df97cdbd494c9f790b9343bc2ca71b4433a8296b
rad <radzhome@gmail.com>
2018-07-26 19:00:17 -0400

    fix syntax error

--- djongo/sql2mongo/query.py
+++ djongo/sql2mongo/query.py
@@ -284,8 +284,9 @@ class SelectQuery(Query):
                 if selected.table == self.left_table:
                     try:
                         # TODO: Path to fix string datetimes.. sorry ugly hack
+                        col_val = doc[selected.table][selected.column]
                         if (not self.connection_properties.enforce_schema and isinstance(doc[selected.column], str) and
-                                (doc[selected.column].endswith('Z') or (len(doc[selected.column) > 2 and doc[selected.column][-3] == ':'))):
+                                (col_val.endswith('Z') or (len(col_val) > 2 and col_val[-3] == ':'))):
                             try:
                                 import dateutil.parser
                                 doc[selected.column] = dateutil.parser.parse(doc[selected.column]).replace(tzinfo=None)
